ef38009299e5f9ede2766241e1c1915d3b7b8b71
hatanooh <hatanooh@gmail.com>
2017-07-23 14:32:17 +0800

    删除dist目录，增加加载字体，不过貌似无效

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+
+    var a = document.createElement('div')
+    a.innerHTML = _.join(['Hello', 'webpack', '方正隶书（貌似无效）'], ' ');
+    a.classList.add('hello');
+    element.appendChild(a)
+
+    var e = document.createElement('div')
+    e.innerHTML = "普通字体：方正隶书";
+    element.appendChild(e)
 
     var myIcon = new Image();
     myIcon.src = Icon;
--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,24 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:[
+                test: /\.css$/,
+                use: [
                     'style-loader',
                     'css-loader'
                 ]
             },
             {
-                test:/\.(png|svg|jpg|gif)$/,
-                use:[
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
                     'file-loader'
                 ]
             }
